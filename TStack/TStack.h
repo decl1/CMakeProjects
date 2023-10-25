fa2fa344e8364b2252a4a557577c7448e8180564
decl1 <zengaming1122@mail.ru>
2023-10-25 15:54:41 +0300

    cpplint changes 2

--- TStack/TStack.h
+++ TStack/TStack.h
@@ -1,62 +1,52 @@
 // Copyright 2023 Anikin Maksim
 
-#ifndef TSTACK_H_
-#define TSTACK_H_
+#ifndef TSTACK_TSTACK_H_
+#define TSTACK_TSTACK_H_
 
 template <class T>
-class TStack
-{
+class TStack {
     size_t size;
     T* data;
     size_t _top;
     TStack() : size(0), data(nullptr), _top(-1) { }
 
 public:
-    TStack(siez_t _size) : size(_size), _top(-1)
-    {
+
+    TStack(siez_t _size) : size(_size), _top(-1) {
         data = new T[size];
     } 
-    TStack(const TStack& st) : size(st.size), _top(st._top) 
-    {
+    TStack(const TStack& st) : size(st.size), _top(st._top) {
         std::copy(data, st.data);
     }
-    ~TStack()
-    {
+    ~TStack() {
         delete[] data;
         data = nullptr;
     }
 
-    bool isFull()
-    {
+    bool isFull() {
         return size == _top + 1;
     }
-    bool isEmpty()
-    {
+    bool isEmpty() {
         return _top == -1;
     }
-    T& top()
-    {
+    T& top() {
         return data[_top];
     }
-    void pop()
-    {
+    void pop() {
         if(isEmpty())
             throw;
         top--;
     }
-    void push(T& n)
-    {
+    void push(T& n) {
         if(isFull)
             throw;
         data[++_top] = n;
     }
-    void Print()
-    {
-        for(int i = _top; i > -1;i--)
-        {
+    void Print() {
+        for(int i = _top; i > -1;i--) {
             std::cout << i << " : " << data[i] << std::endl;
         }
     }
 };
 
-#endif // TSTACK_H_
+#endif // TSTACK_TSTACK_H_
