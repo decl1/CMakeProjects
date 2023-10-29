79f58c956394dabebc419a1f78f1274da589094a
decl1 <zengaming1122@mail.ru>
2023-10-29 20:13:57 +0300

    TList some dev

--- TList/TList.h
+++ TList/TList.h
@@ -3,4 +3,21 @@
 #ifndef TLIST_TLIST_H_
 #define TLIST_TLIST_H_
 
+template <class T>
+class TNode {
+	T data;
+	TNode* next;
+ public:
+	TNode(const TNode& node) : data(node.data), next(node.next) { };
+    TNode(T _data, TNode* _next = nullptr) : data(_data), next(_next) { };
+    ~TNode() { delete[] next; };
+};
+
+template <class T>
+class TList {
+	friend class TNode;
+ public:
+	 
+};
+
 #endif  // TLIST_TLIST_H_
