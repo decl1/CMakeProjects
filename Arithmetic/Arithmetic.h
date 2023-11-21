d1be43815aa11eadecc0e28731dca89088308a5f
decl1 <77211193+decl1@users.noreply.github.com>
2023-11-22 01:51:59 +0300

    add menu print

--- Arithmetic/Arithmetic.h
+++ Arithmetic/Arithmetic.h
@@ -3,6 +3,16 @@
 #ifndef ARITHMETIC_ARITHMETIC_H_
 #define ARITHMETIC_ARITHMETIC_H_
 
+#include <iostream>
 
+void printmenus() {
+    std::cout << "Меню:" << std::endl;
+    std::cout << "\t1. Задать значение переменной" << std::endl;
+    std::cout << "\t2. Увидеть обратную польскую запись" << std::endl;
+    std::cout << "\t3. Вычислить значение функции" << std::endl;
+    std::cout << "\t4. Вернуться к заданию функции" << std::endl;
+    std::cout << "\t5. Выход" << std::endl << std::endl;
+    std::cout << "Выбор: ";
+}
 
 #endif ARITHMETIC_ARITHMETIC_H_
--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,21 @@
 // Copyright 2023 Anikin Maskim
 
 #include <iostream>
+#include <Windows.h>
+#include "../Arithmetic/Arithmetic.h"
 
 int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    while (1) {
+        int menuswitch = 0;
+        printmenus();
+        std::cin >> menuswitch;
+        switch (menuswitch) {
+            default: {
+                system("cls");
+                continue;
+            }
+        }
+    }
 }
