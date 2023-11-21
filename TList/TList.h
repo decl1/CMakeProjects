410dc5c7be79bd59b09967779790c6c57a3d6446
decl1 <77211193+decl1@users.noreply.github.com>
2023-11-21 23:43:45 +0300

    some tabs fix

--- TList/TList.h
+++ TList/TList.h
@@ -8,9 +8,9 @@ class TNode {
 	T data;
 	TNode* next;
  public:
-	TNode(const TNode& node) : data(node.data), next(node.next) { };
-	TNode(T _data, TNode* _next = nullptr) : data(_data), next(_next) { };
-	~TNode() { delete[] next; };
+	TNode(const TNode& node) : data(node.data), next(node.next) { }
+	TNode(T _data, TNode* _next = nullptr) : data(_data), next(_next) { }
+	~TNode() { delete[] next; }
 };
 
 template <class T>
@@ -19,7 +19,7 @@ class TList {
 	TNode* first;
 	TNode* last;
  public:
-	TList() : first(nullptr), last(nullptr) { };
+	TList() : first(nullptr), last(nullptr) { }
 	bool isEmpty() {
 		return first == nullptr;
 	}
