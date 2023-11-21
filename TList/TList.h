810140b65914950b8723c7a2db7b6c32dd129d30
decl1 <77211193+decl1@users.noreply.github.com>
2023-11-21 23:14:36 +0300

    tlist

--- TList/TList.h
+++ TList/TList.h
@@ -8,7 +8,7 @@ class TNode {
 	T data;
 	TNode* next;
  public:
-	TNode(const TNode& node) : data(node.data), next(node.next) { };
+    TNode(const TNode& node) : data(node.data), next(node.next) { };
     TNode(T _data, TNode* _next = nullptr) : data(_data), next(_next) { };
     ~TNode() { delete[] next; };
 };
@@ -16,8 +16,65 @@ class TNode {
 template <class T>
 class TList {
 	friend class TNode;
+	TNode* first;
+	TNode* last;
  public:
-	 
-};
+	 list() : first(nullptr), last(nullptr) { };
+	 bool isEmpty() {
+		 return first == nullptr;
+	 }
+	 void pushback(T data) {
+		 TNode<T>* p = new TNode<T>(data);
+		 if (isEmpty()) {
+			 first = p;
+			 last = p;
+			 return;
+		 }
+		 last->next = p;
+		 last = p;
+	 }
+	 void print() {
+		 if (isEmpty()) return;
+		 TNode<T>* p = first;
+		 while (p) {
+			 cout << p->data << " ";
+			 p = p->next;
+		 }
+		 cout << endl;
+	 }
+	 TNode<T>* find(T _data) {
+		 if (isEmpty()) return nullptr;
+		 TNode<T>* p = first;
+		 while (p && p->data != _data) p = p->next;
+		 return (p && p->data != _data) ? p : nullptr;
+	 }
+	 void popfirst() {
+		 if (isEmpty()) return;
+		 TNode<T>* p = first;
+		 first = p->next;
+		 delete p;
+	 }
+	 void poplast() {
+		 if (isEmpty()) return;
+		 if (first == last) {
+			 popfirst();
+			 return;
+		 }
+		 TNode<T>* p = first;
+		 while (p->next != last) p = p->next;
+		 p->next = nullptr;
+		 delete last;
+		 last = p;
+	 }
+	 TNode<T>* operator[] (const int index) {
+		 if (is_empty()) return nullptr;
+		 TNode<T>* p = first;
+		 for (int i = 0; i < index; i++) {
+			 p = p->next;
+			 if (!p) return nullptr;
+		 }
+		 return p;
+	 }
+}; 
 
 #endif  // TLIST_TLIST_H_
