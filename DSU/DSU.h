48bc399c5e11ac55e221f9b7faac1be1f6c58ef3
decl1 <zengaming1122@mail.ru>
2023-11-22 14:50:39 +0300

    dsu file system

---
+++ DSU/DSU.h
@@ -0,0 +1,8 @@
+// Copyright 2023 Anikin Maksim
+
+#ifndef DSU_DSU_H_
+#define DSU_DSU_H_
+
+
+
+#endif DSU_DSU_H_
---
+++ tests/test_DSU.cpp
@@ -0,0 +1,4 @@
+// Copyright 2023 Anikin Maksim
+
+#include <gtest.h>
+#include "../DSU/DSU.h"
