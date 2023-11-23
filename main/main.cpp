a53f238a5018d130e84139d2086aa874c02556f8
decl1 <77211193+decl1@users.noreply.github.com>
2023-11-23 15:51:41 +0300

    arithmetic main added

--- main/main.cpp
+++ main/main.cpp
@@ -1,27 +1,6 @@
 // Copyright 2023 Anikin Maskim
 
 #include <iostream>
-#include <Windows.h>
-#include "../Arithmetic/Arithmetic.h"
 
 int main() {
-    // Settings
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    // Vars
-    std::string equation = "";
-    bool eqflag = 0;
-    // Code
-    equation_enter(equation);
-    while (1) {
-        int menuswitch = 0;
-        printmenus(equation);
-        std::cin >> menuswitch;
-        switch (menuswitch) {
-            default: {
-                system("cls");
-                continue;
-            }
-        }
-    }
 }
